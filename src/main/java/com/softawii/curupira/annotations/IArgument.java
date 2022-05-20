2a60e28e681201793b4901d481ba51eac2ec36b9
yaansz <yancfm@gmail.com>
2022-05-19 21:26:25 -0300

    feat: docs + hiding internal features #7

--- src/main/java/com/softawii/curupira/annotations/IArgument.java
+++ src/main/java/com/softawii/curupira/annotations/IArgument.java
@@ -4,13 +4,38 @@ import net.dv8tion.jda.api.interactions.commands.OptionType;
 
 import java.lang.annotation.*;
 
+/**
+ * Annotation for the argument of a command.
+ * <p>
+ *     The annotation is used to define the argument of a command.
+ *     It will be used by the {@link com.softawii.curupira.annotations.ICommand} class to
+ *     create the command.
+ *
+ *     The {@link com.softawii.curupira.annotations.ICommand} class will be detected by
+ *     {@link com.softawii.curupira.core.Curupira} class and will be tracked by OnSlashCommandInteraction.
+ * </p>
+ *
+ * @version 1.0.0
+ * @author yaansz
+ */
 @Target(ElementType.METHOD)
 @Retention(RetentionPolicy.RUNTIME)
 @Repeatable(IArguments.class)
 public @interface IArgument {
 
+    /**
+     * This is a choice of the argument.
+     *
+     * <p>
+     *     It is used to define if we will recommend you some possibilities, if {@link com.softawii.curupira.annotations.IArguments}.hasAutoComplete
+     *     else, it can be used to define specific options for the argument.
+     * </p>
+     *
+     * @version 1.0.0
+     * @author yaansz
+     */
     @Retention(RetentionPolicy.RUNTIME)
-    @interface Choice {
+    @interface IChoice {
         String key();
         String value() default "";
     }
@@ -19,5 +44,5 @@ public @interface IArgument {
     boolean required()        default false;
     OptionType type()         default OptionType.STRING;
     boolean hasAutoComplete() default false;
-    Choice[] choices()        default {};
+    IChoice[] choices()        default {};
 }
--- src/main/java/com/softawii/curupira/annotations/IGroup.java
+++ src/main/java/com/softawii/curupira/annotations/IGroup.java
@@ -5,6 +5,16 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * Annotation to mark a class as a group.
+ *
+ * <p>
+ *     The annotation is not related to JDA, it's the way to track groups internally of the library and create the help command.
+ * </p>
+ *
+ * @author yaansz
+ * @version 1.0.0
+ */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE)
 public @interface IGroup {
