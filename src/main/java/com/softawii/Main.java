bf0a35b05e02c58a82bce46ddcbee1c479f5022f
yaansz <yancfm@gmail.com>
2024-10-07 19:15:24 -0300

    feat: guild only #20

--- src/main/java/com/softawii/Main.java
+++ src/main/java/com/softawii/Main.java
@@ -6,8 +6,16 @@ import com.softawii.curupira.v2.core.CurupiraBoot;
 import com.softawii.curupira.v2.integration.BasicContextProvider;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.requests.GatewayIntent;
+import net.dv8tion.jda.api.utils.MemberCachePolicy;
+import net.dv8tion.jda.api.utils.cache.CacheFlag;
+import okhttp3.Cache;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class Main {
 
@@ -24,9 +32,14 @@ public class Main {
         context.registerInstance(ComplexController.class, new ComplexController());
         context.registerInstance(TranslatedController.class, new TranslatedController());
         context.registerInstance(AutoMenuController.class, new AutoMenuController());
+        context.registerInstance(AdminController.class, new AdminController());
 
-        JDABuilder builder = JDABuilder.createDefault(token);
+        JDABuilder builder = JDABuilder.createDefault(token)
+                .enableCache(CacheFlag.CLIENT_STATUS, CacheFlag.ONLINE_STATUS)
+                .enableIntents(GatewayIntent.GUILD_PRESENCES, GatewayIntent.GUILD_MEMBERS)
+                .setMemberCachePolicy(MemberCachePolicy.ALL);
         JDA JDA = builder.build();
+        JDA.awaitReady();
 
         boolean reset = false;
         CurupiraBoot curupira = new CurupiraBoot(JDA, context, reset, pkg);
---
+++ src/main/java/com/softawii/example/controller/AdminController.java
@@ -0,0 +1,80 @@
+package com.softawii.example.controller;
+
+import com.softawii.curupira.v2.annotations.DiscordController;
+import com.softawii.curupira.v2.annotations.commands.DiscordCommand;
+import com.softawii.curupira.v2.annotations.interactions.DiscordButton;
+import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.Permission;
+import net.dv8tion.jda.api.entities.Guild;
+import net.dv8tion.jda.api.entities.MessageEmbed;
+import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
+import net.dv8tion.jda.api.events.interaction.component.ButtonInteractionEvent;
+import net.dv8tion.jda.api.interactions.callbacks.IReplyCallback;
+import net.dv8tion.jda.api.interactions.components.buttons.Button;
+
+import java.awt.*;
+import java.util.List;
+
+@DiscordController(value = "admin", description = "hello!", guildId = 856534404779868180L, permissions = Permission.ADMINISTRATOR)
+public class AdminController {
+
+    private static final int guildPageSize = 5;
+    private static final String adminGuildNextPage = "admin-guild-next-page";
+    private static final String adminGuildPrevPage = "admin-guild-prev-page";
+
+    private MessageEmbed paginateGuilds(List<Guild> guilds, int page) {
+        int maxPages = (int) Math.ceil((double) guilds.size() / AdminController.guildPageSize);
+
+        if(page < 0) {
+            page = 0;
+        } else if(page >= maxPages) {
+            page = maxPages - 1;
+        }
+
+        // Get the guilds for the current page
+        int start = page * AdminController.guildPageSize;
+        int end = Math.min(start + AdminController.guildPageSize, guilds.size());
+        List<Guild> guildsPage = guilds.subList(start, end);
+
+        EmbedBuilder embed = new EmbedBuilder()
+                .setTitle("Guilds (Page " + (page + 1) + "/" + maxPages + ")")
+                .setColor(Color.CYAN);
+
+        for (Guild guild : guildsPage) {
+            embed.addField(guild.getName(), "ID: " + guild.getId(), false);
+        }
+
+        return embed.build();
+    }
+
+    private void sendGetGuildsResponse(IReplyCallback event, JDA jda, int page) {
+        int maxPages = (int) Math.ceil((double) jda.getGuilds().size() / AdminController.guildPageSize);
+
+        Button next = Button.primary(adminGuildNextPage + ':' + (page + 1), "Next").withDisabled(page == maxPages - 1);
+        Button prev = Button.primary(adminGuildPrevPage + ':' + (page - 1), "Previous").withDisabled(page == 0);
+
+        MessageEmbed embed = paginateGuilds(jda.getGuilds(), page);
+
+        event.replyEmbeds(embed).addActionRow(prev, next).setEphemeral(true).queue();
+    }
+
+    @DiscordCommand(name = "guilds", description = "list all guilds", ephemeral = true)
+    public void getGuildsCommand(SlashCommandInteractionEvent event, JDA jda) {
+        sendGetGuildsResponse(event, jda, 0);
+    }
+
+    @DiscordButton(name = adminGuildPrevPage)
+    public void getPreviousPage(ButtonInteractionEvent event, JDA jda) {
+        int page = Integer.parseInt(event.getComponentId().split(":")[1]);
+        sendGetGuildsResponse(event, jda, page);
+        event.getMessage().delete().queue();
+    }
+
+    @DiscordButton(name = adminGuildNextPage)
+    public void getNextPage(ButtonInteractionEvent event, JDA jda) {
+        int page = Integer.parseInt(event.getComponentId().split(":")[1]);
+        sendGetGuildsResponse(event, jda, page);
+        event.getMessage().delete().queue();
+    }
+}
